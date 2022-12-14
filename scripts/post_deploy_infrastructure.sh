9851796e8acbe00bbe19c1f2b18a40dc26da8350
George Cooper <george@marlincode.com>
2022-12-13 20:34:46 -0500

    better scripts

--- scripts/post_deploy_infrastructure.sh
+++ scripts/post_deploy_infrastructure.sh
@@ -5,9 +5,9 @@ cloudFrontId="${cloudFrontId#\"}"
 domainName=$(aws cloudfront get-distribution --id $cloudFrontId --query Distribution.DomainName)
 bucketName=$(aws cloudformation describe-stack-resource --stack-name $npm_package_config_cloudFormationStackName --logical-resource-id WebsiteBucket --query StackResourceDetail.PhysicalResourceId)
 
-YELLOW='\033[1;33m'
+GREEN='\033[1;42m'
 NC='\033[0m' # No Color
-echo -e "${YELLOW}Please copy this bucket name into the config.deploymentBucket property in your package.json file${NC}"
+echo -e "${GREEN}Please copy this bucket name into the config.deploymentBucket property in your package.json file${NC}"
 echo "BucketName": $bucketName
 echo -e "\n\n"
 echo "After building and deploying your app, it will be available at this url:"
