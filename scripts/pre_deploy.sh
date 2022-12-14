de6fd6724fb73dce381666600a3d249df856cfee
George Cooper <george@marlincode.com>
2022-12-13 20:09:18 -0500

    template ready to go

--- scripts/pre_deploy.sh
+++ scripts/pre_deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-if [ "$npm_package_config_deploymentBucket" = "" ]
+if [ "$npm_package_config_cloudFormationStackName" = "" ]
+then
+  echo "Stack name not configured, please insert bucket name in package.json"
+  exit 100
+elif [ "$npm_package_config_deploymentBucket" = "" ]
 then
   echo "Deployment bucket not configured, please insert bucket name in package.json"
   exit 100
